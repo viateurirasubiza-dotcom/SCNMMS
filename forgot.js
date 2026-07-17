82da616ddd5cc91b8c4d2a27befb39aec3fcbb51
viateurirasubiza-dotcom <viateurirasubiza@gmail.com>
2026-07-17 11:55:43 +0200

    Update API endpoint and refactor resetPassword function

--- forgot.js
+++ forgot.js
@@ -1,77 +1,56 @@
-const API =
-"http://127.0.0.1:5000";
-
-
-
+const API = "https://scnmms.onrender.com";
 
 
 async function resetPassword(){
 
+    let username =
+    document.getElementById("username").value;
 
 
-let username =
-document.getElementById(
-"username"
-).value;
-
-
-
-let password =
-document.getElementById(
-"newPassword"
-).value;
+    let password =
+    document.getElementById("newPassword").value;
 
 
 
+    let response = await fetch(
+
+        API + "/forgot-password",
+
+        {
+
+            method:"PUT",
+
+            headers:{
+
+                "Content-Type":"application/json"
+
+            },
 
 
-let response = await fetch(
+            body:JSON.stringify({
 
-API+"/forgot-password",
+                username:username,
 
-{
+                password:password
 
-method:"PUT",
+            })
 
-headers:{
+        }
 
-"Content-Type":
-"application/json"
-
-},
+    );
 
 
-body:JSON.stringify({
-
-username:username,
-
-password:password
-
-})
+    let data = await response.json();
 
 
-}
-
-);
-
-
-
-
-
-let data =
-await response.json();
+    alert(data.message);
 
 
 
-alert(data.message);
+    if(data.success){
 
+        window.location.href="index.html";
 
-
-if(data.success){
-
-window.location.href="index.html";
-
-}
-
+    }
 
 }
